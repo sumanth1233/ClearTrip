e086e8e054da7e6e0b24ece4eedcd0b44d010af1
santoshss24 <66872766+santoshss24@users.noreply.github.com>
2020-12-06 12:59:51 +0530

    Added TestCase TC003

---
+++ sampleCleartrip/src/test/java/testcases/TC003.java
@@ -0,0 +1,15 @@
+package testcases;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC003 extends BaseTest
+{
+	@Test
+	public void deleteUser()
+	{
+		
+		
+	}
+
+}
